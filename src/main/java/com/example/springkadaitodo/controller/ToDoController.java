4b90e5554eb171dee633a9da1c17e6ee96bc98c7
gin3202 <doumoriso.fps870@gmail.com>
2025-08-07 15:13:07 +0900

    Initial commit

---
+++ src/main/java/com/example/springkadaitodo/controller/ToDoController.java
@@ -0,0 +1,24 @@
+package com.example.springkadaitodo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.springkadaitodo.service.ToDoService;
+
+@Controller
+public class ToDoController {
+
+    private final ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping("/todo")
+    public String showTodoList(Model model) {
+        model.addAttribute("todoList", toDoService.getAllTodos());
+        return "todoView";
+    }
+}
+
